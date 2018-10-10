996999dde6ab883a0a7c50b8b919c77cc6805900
problitz <morzel41@gmail.com>
2018-10-10 18:10:51 -0400

    added collision and disconnect fix

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -17,11 +16,21 @@ http.listen(port, function(){
 });
 
 var players = {};
-io.on('connection', function(socket) {
+var socket_list = new Array();
+var placementX=0;
+var placementY=0;
+
+io.on('connection', function(socket) { //changed placement so i don't need to move the box before opening it in a new tab
+  socket_list.push(socket.id);
+  placementX+=200;
+  if(placementX>1000){
+    placementX=200;
+    placementY+=300;
+  }
   socket.on('new player', function() {
     players[socket.id] = {
-      x: 300,
-      y: 300
+      x: placementX,
+      y: placementY
     };
   });
   socket.on('input', function(data) {
@@ -38,9 +47,47 @@ io.on('connection', function(socket) {
     if (data.down && player.y < data.canvasy- 100) {
       player.y += 5;
     }
+
+    var i;
+    var j;
+
+    for(var i = 0; i < socket_list.length; i++) { 
+      for(var j = 0; j < socket_list.length; j++) { 
+      var collide1 = players[socket_list[i]] || {};
+      var collide2 = players[socket_list[j]] || {};
+
+      if(socket_list[j] == socket_list[i]){
+        break;
+      }
+
+      if(players[socket_list[i]].x+100 > players[socket_list[j]].x && players[socket_list[i]].x < players[socket_list[j]].x+100 && players[socket_list[i]].y+100 > players[socket_list[j]].y && players[socket_list[i]].y < players[socket_list[j]].y+100){
+        //change what you want collision to do here (box 1 hits box 2)
+        collide1.x=200;
+        collide1.y=200;
+        collide2.x=400;
+        collide2.y=400;
+      }
+      if(players[socket_list[j]].x >= players[socket_list[i]].x && players[socket_list[j]].x <= players[socket_list[i]].x+100 && players[socket_list[j]].y >= players[socket_list[i]].y && players[socket_list[j]].y <= players[socket_list[i]].y+100){
+         //change what you want collision to do here (box 2 hits box 1)
+        collide1.x=200;
+        collide1.y=200;
+        collide2.x=400;
+        collide2.y=400;
+      }
+    }
+  }
+  
   });
   socket.on('disconnect', function() {
+    var i;
     delete players[socket.id];
+       for(var i = 0; i < socket_list.length; i++) { 
+        if(socket.id == socket_list[i]){
+          socket_list.splice(i, 1);
+          i=socket_list.length+2;
+        }
+       }
+
   });
 });
 setInterval(function() {
